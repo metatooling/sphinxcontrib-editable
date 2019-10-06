27436dbc4d63946807d3ca90529f72a3ed19b714
interdoc <interdoc@cordaz.com>
2019-10-06 20:27:20 +0000

    Add namepace package

--- setup.py
+++ setup.py
@@ -5,7 +5,8 @@ import setuptools
 
 setuptools.setup(
     name="sphinxcontrib-editable",
-    version="0.0.1",
+    version="0.0.3",
     packages=setuptools.find_packages(),
+    namespace_packages=['sphinxcontrib'],
     include_package_data=True,
 )
